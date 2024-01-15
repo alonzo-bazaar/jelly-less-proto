d3fb9ff67acd88a46deef3cceac31f31ef2012bd
alozo-bazaar <dewey.hexadecimal.system@protonmail.com>
2024-01-15 14:29:29 +0100

    fix all assertion orders

--- src/test/java/org/jelly/utils/IteratorCollectorTest.java
+++ src/test/java/org/jelly/utils/IteratorCollectorTest.java
@@ -12,23 +12,23 @@ public class IteratorCollectorTest {
     @Test
     public void arrayListCollect() {
         ArrayList<Character> al = IteratorCollector.collectToArray(new StringCharIterator("cristo"));
-        assertEquals((char)al.get(0), 'c');
-        assertEquals((char)al.get(1), 'r');
-        assertEquals((char)al.get(2), 'i');
-        assertEquals((char)al.get(3), 's');
-        assertEquals((char)al.get(4), 't');
-        assertEquals((char)al.get(5), 'o');
+        assertEquals('c', (char)al.get(0));
+        assertEquals('r', (char)al.get(1));
+        assertEquals('i', (char)al.get(2));
+        assertEquals('s', (char)al.get(3));
+        assertEquals('t', (char)al.get(4));
+        assertEquals('o', (char)al.get(5));
     }
 
     @Test
     public void linkedListCollect() {
         LinkedList<Character> li = IteratorCollector.collectToLinked(new StringCharIterator("cristo"));
-        assertEquals((char)li.get(0), 'c');
-        assertEquals((char)li.get(1), 'r');
-        assertEquals((char)li.get(2), 'i');
-        assertEquals((char)li.get(3), 's');
-        assertEquals((char)li.get(4), 't');
-        assertEquals((char)li.get(5), 'o');
+        assertEquals('c', (char)li.get(0));
+        assertEquals('r', (char)li.get(1));
+        assertEquals('i', (char)li.get(2));
+        assertEquals('s', (char)li.get(3));
+        assertEquals('t', (char)li.get(4));
+        assertEquals('o', (char)li.get(5));
     }
 
     @Test
