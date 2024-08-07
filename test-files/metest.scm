e9074f16a90c424990a10918abfe319351164a66
alozo-bazaar <alonzo.lo.stronzo@protonmail.com>
2024-08-07 16:52:06 +0200

    cleanup

--- test-files/metest.scm
+++
@@ -1,4 +0,0 @@
-#|
-"fork" of test.scm meant to test the (very) limited subset of scheme
-that I actually managed to implement
-|#
--- test-files/stdlib/stdlib.scm
+++
@@ -1,3 +0,0 @@
-;; todo, handle loading file from file
-;; all relative filepaths are currently considered from the cwd of the running process
-;; change that plz? (set a cwd in the runtime and use that to evaluate relative paths?)
